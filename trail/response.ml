56089623ec15feddb259150bba988b129d81529a
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-02 13:48:14 +0100

    feat: respect a users content-length headers

--- trail/response.ml
+++ trail/response.ml
@@ -18,15 +18,16 @@ let to_buffer { status; headers; version; body } =
   let version = version |> Http.Version.to_string |> Httpaf.Version.of_string in
   let status = status |> Http.Status.to_int |> Httpaf.Status.of_code in
 
+  let content_length = Http.Header.get headers "content-length" in
   let headers =
-    match body with
-    | Some body ->
+    match (body, content_length) with
+    | Some body, None ->
         let content_length =
           Http.Header.get headers "content-length"
           |> Option.value ~default:(IO.Buffer.length body |> Int.to_string)
         in
         Http.Header.add headers "content-length" content_length
-    | None -> headers
+    | _ -> headers
   in
 
   let headers = headers |> Http.Header.to_list |> Httpaf.Headers.of_list in
